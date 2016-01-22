3c45aa249af884cbd8435ae4a25bb14e65899ad6
Charles Kibue <charles.kibue@soulfx.com>
2016-01-22 14:16:01 -0500

    Added Grunt/Bower config

---
+++ Gruntfile.js
@@ -0,0 +1,76 @@
+module.exports = function(grunt){
+	require('matchdep').filterDev('grunt-*', './package.json').forEach(grunt.loadNpmTasks);
+	// grunt.loadNpmTasks('grunt-sass');
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		// Grunt Watch
+		watch: {
+			options: {
+		      reload: true,
+		      atBegin: true
+		    },
+		    css: {
+		    	files: [
+		    		'sass/*.scss',
+		    		'sass/*/*.scss'
+		    	],
+		    	tasks: ['sass', 'postcss']
+		    },
+            init: {
+                files: [
+                    'bower_components/*',
+                    'bower_components/*/*',
+                ],
+                tasks: ['copy']
+            }
+        },
+
+        // Task - Compile SASS
+        sass: {
+        	options: {
+        		sourceMap: true
+        	},
+        	dev: {
+        		files: {
+        			'css/styles.css': 'sass/styles.scss'
+        		}
+        	}
+        },
+
+        // Task - PostCSS
+        postcss: {
+        	options: {
+        		map: true,
+        		processors: [
+                    require('autoprefixer')({
+                        browsers: ['last 2 versions']
+                    })
+                ]
+        	},
+        	dev: {
+        		src: 'css/*.css'
+        	}
+        },
+
+        // Task - Copy over Vendor Scripts
+        copy: {             
+            customSass: {
+                files: [
+                    {
+                        src: 'bower_components/sass-custom/sass/custom/**',
+                        dest: 'sass/custom/',                        
+                        expand: true,
+                        flatten: true,                         
+                        filter: 'isFile'                
+                    }
+                ]                
+            }
+        }
+
+	});	// grunt.initConfig()
+
+    grunt.registerTask('default', []);  
+	grunt.registerTask('init', ['copy']);	
+};
