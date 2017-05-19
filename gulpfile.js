f7aef1aa9b6c80f182adb32fe5fea1f696a71fc3
Charles Kibue <charles.kibue@soulfx.com>
2017-05-19 17:33:38 -0400

    Config: Gulp

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+/*Config*/
+var gulp = require( 'gulp' );
+var sourceMaps = require( 'gulp-sourcemaps' );
+var runSequence = require( 'run-sequence' );
+var rename = require( 'gulp-rename' );
+/*SASS/CSS*/
+var sass = require( 'gulp-sass' );
+var autoPrefixer = require( 'gulp-autoprefixer' );
+var cssMin = require( 'gulp-cssnano' );
+
+
+/*Config*/
+var vendorCssFiles = [
+    'node_modules/normalize.css/normalize.css'
+];
+
+gulp.task( 'watch', function () {
+    gulp.watch( 'sass/**/*.scss', [ 'css' ] );
+} );
+
+gulp.task( 'build', function ( callback ) {
+    return runSequence(
+        'vendorStyles',
+        'css'
+    );
+} );
+
+gulp.task( 'vendorStyles', function () {
+    return gulp.src( vendorCssFiles )
+        .pipe( concat( '_vendor.scss' ) )
+        .pipe( gulp.dest( 'sass/vendors/' ) );
+} );
+
+gulp.task( 'css', function () {
+    return gulp.src( 'sass/main.scss' )
+        .pipe( sass()
+            .on( 'error', sass.logError ) )
+        .pipe( sourceMaps.init() )
+        .pipe( autoPrefixer( {
+            browsers: [ 'last 2 versions' ],
+            cascade: false
+        } ) )
+        .pipe( sourceMaps.write( '.' ) )
+        .pipe( gulp.dest( 'Stylesheets' ) );
+} );
